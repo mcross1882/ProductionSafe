70e9f9c738238212188d732600b0cc74ad456757
Matthew Cross <matthew@koddi.com>
2016-10-07 15:12:03 -0500

    Implement disable for 5 minutes

--- js/ps-inject-banner.js
+++ js/ps-inject-banner.js
@@ -1,17 +1,30 @@
 (function() {
 
   var Injector = function() {
-
+    this.MAX_TIMEOUT = 1000 * 60 * 5; // 5 minutes
   }
 
   Injector.prototype.showBanner = function() {
-    if (!this.hasBanner()) {
-        document.body.appendChild(this.buildBanner());
+    if (!this.hasBanner() && !this.isBannerDisabled()) {
+      document.body.appendChild(this.buildBanner());
     }
   }
 
   Injector.prototype.hasBanner = function() {
     return null != document.querySelector(".production-safe-banner-wrapper");
+  }
+
+  Injector.prototype.isBannerDisabled = function() {
+    var rawTime = localStorage.getItem('ps-inject-banner-timeout');
+    if (!rawTime) {
+      return false;
+    }
+
+    var startTime = new Date(rawTime);
+    var endTime = new Date();
+    var ellapsedTime = endTime - startTime;
+
+    return ellapsedTime < this.MAX_TIMEOUT;
   }
 
   Injector.prototype.buildBanner = function() {
@@ -21,12 +34,26 @@
       wrapper.parentNode.removeChild(wrapper);
     });
 
+    var disableButton = this.createButton('Disable for 5 minutes');
+    disableButton.setAttribute("class", "btn-timeout");
+    disableButton.onclick = function() {
+      localStorage.setItem('ps-inject-banner-timeout', (new Date()).toISOString());
+    };
+
     var border = document.createElement("div");
     border.setAttribute("class", "border-wrapper");
-    border.innerHTML = "You are currently on a production site. Watch the fuck out!";
+    border.innerHTML = "<p>You are currently on a production site. Watch the fuck out!</p>";
+    border.appendChild(disableButton);
     wrapper.appendChild(border);
 
     return wrapper;
+  }
+
+  Injector.prototype.createButton = function(title) {
+    var button = document.createElement("button");
+    button.setAttribute("type", "button");
+    button.textContent = title;
+    return button;
   }
 
   var inject = new Injector();
